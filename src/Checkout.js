461dac6cba86fec48b66f50a52e48952471f7679
unknown <pressistephan@gmail.com>
2020-10-31 20:16:11 +0200

    Interface Subtotal is added to ckeckout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal"
+
 
 function Checkout() {
   return (
@@ -7,12 +9,17 @@ function Checkout() {
       <div className="checkout__left">
         <img
           className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G02/UK_CCMO/TM/OCC_Amazon1._CB423492668_.jpg"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+        <div>
+          <h2 className="checkout__title"> Your Shopping Basket </h2>  
+        </div>
       </div>
 
-      <div className="checkout__right"></div>
+      <div className="checkout__right">
+        <Subtotal />
+       </div>
     </div>
   );
 }
