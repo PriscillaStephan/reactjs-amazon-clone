8de92fc8d259e7f080ee738722f72eab689cf142
unknown <pressistephan@gmail.com>
2020-11-02 23:33:23 +0200

    complete the checkoutscreen with React Context API

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,13 @@
      basket: [],
  };
 
+// Selector - reduce function
+export const getBasketTotal = (basket) => 
+    //the way this function work:
+    //everytime i want the item price to add to the total amount,
+    // and the initial total amount is gonna be  0
+    basket?.reduce((amount, item) => item.price + amount, 0)
+
  const reducer = (state, action) => {
      console.log(action);
      switch(action.type) {
@@ -11,6 +18,12 @@
                 basket: [...state.basket, action.item],
             };
 
+        case 'REMOVE_FROM_BASKET':
+            return {
+                ...state,
+                basket: state.basket.filter(item => item.id !== action.id)
+            }
+        
             default:
                 return state;
      } 
